d9dc1362f2736a20e234857ceac0179d74adf97c
JohnnyHsieh <johnny7861532@hotmail.com>
2016-10-04 10:00:44 +0800

    Initial Commit

---
+++ Swift3 test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift3 test
+//
+//  Created by Johnny Hsieh on 2016/10/4.
+//  Copyright © 2016年 PixelMonkey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
