fbd59ecce4e3832ad699dc708fca737b791c9375
JohnnyHsieh <johnny7861532@hotmail.com>
2016-10-17 17:03:02 +0800

    Firebase swift3 testing
    Just make a small app to test how firebase work on Swift3

---
+++ Podfile
@@ -0,0 +1,13 @@
+platform :ios, '9.0'
+
+target 'Swift3 test' do
+ 
+use_frameworks!
+pod 'Firebase'
+pod 'Firebase/Auth’
+pod 'Firebase/Storage’
+pod 'Firebase/Database’
+pod 'GoogleSignIn'
+ 
+
+end
--- Swift3 test/ViewController.swift
+++ Swift3 test/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
-
+import FirebaseDatabase
 class ViewController: UIViewController {
+    let weatherRef = FIRDatabase.database().reference().child("weather")
 
+    @IBOutlet weak var weatherLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        observeData()
+        self.weatherLabel.isHidden = false
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    func observeData(){
+        self.weatherRef.observe(.value, with: { (snapshot) in
+            self.weatherLabel.text = snapshot.value as? String
+        
+        
+        
+        
+        })
+    
+    
+    
+    
+    
+    
     }
 
+    @IBAction func tapDidChangeSunny(_ sender: AnyObject) {
+        self.weatherRef.setValue("Sunny")
+        print(self.weatherLabel.text)
+    }
+    @IBAction func tapDidChangeRainning(_ sender: AnyObject) {
+        self.weatherRef.setValue("Rainning")
+        print(self.weatherLabel.text)
 
-}
+    }
+    }
 
